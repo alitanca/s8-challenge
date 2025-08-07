459a32632eefb1290444152639410ec65bc9b324
alitanca <tancaali@gmail.com>
2025-08-07 15:59:50 +0300

    home page done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
+// src/components/Home.jsx
+import React from "react";
 import { Link } from "react-router-dom";
-import logo from "/src/components/images/iteration-1-images/logo.svg";
-import banner from "/src/components/images/iteration-1-images/home-banner.png";
-import './Home.css';
+import "./Home.css";
+import logo from "../components/images/iteration-1-images/logo.svg";
+import banner from "../components/images/iteration-1-images/home-banner.png";
 
 export default function Home() {
   return (
     <div className="home-page">
-      <img src={logo} alt="Logo" className="home-logo" />
+      <img src={logo} alt="logo" className="home-logo" />
+      <p className="home-slogan">KOD ACIKTIRIR<br /> PIZZA DOYURUR</p>
 
       <div
         className="home-banner-wrapper"
         style={{ backgroundImage: `url(${banner})` }}
       >
-        <Link to="/pizza">
+        <Link to="/order-pizza">
           <button className="home-button">ACIKTIM</button>
         </Link>
       </div>
     </div>
   );
 }
+
